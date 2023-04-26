a04e00db1eabb50a955e9df94a1996818eadc9cb
giammetta-vasco <giammetta.vasco@iisgalvanimi.edu.it>
2023-04-26 10:37:50 +0000

    updsate

---
+++ inutili/tabels.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="Animali"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE Mammiferi (name VARCHAR(255), specie VARCHAR(255))")

d9023cfcc7b6a0be43741d00fac06ae21e2ab76e
giammetta-vasco <giammetta.vasco@iisgalvanimi.edu.it>
2023-04-27 10:43:37 +0000

    Versione

--- import_data.py
+++ import_data.py
@@ -9,28 +9,23 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-#Create the DB (if not already exists)
-mycursor.execute("CREATE DATABASE IF NOT EXISTS CLASH_ROYALE")
 
 #Create the table for the csv data (if not exists)
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS CLASH_ROYALE.Clash_Unit (
-    Unit VARCHAR(30) NOT NULL,
-    Cost INTEGER,
-    Hit_Speed VARCHAR(30),
-    Speed VARCHAR(30),
-    Deploy_Time VARCHAR(30),
-    Range_ VARCHAR(30),
-    Target VARCHAR(30),
-    Count VARCHAR(30),
-    Transport VARCHAR(30),
-    Type VARCHAR(30),
-    Rarity VARCHAR(30),
-    PRIMARY KEY (Unit)
+  CREATE TABLE IF NOT EXISTS Calciatori-Milan (
+    Cognome VARCHAR(30) NOT NULL,
+    Nome VARCHAR(30) NOT NULL,
+    Ruolo VARCHAR(30) NOT NULL,
+    Numero INTEGER,
+    Gol segnati INTEGER,
+    Assist INTEGER,
+    Valore FLOAT,
+    Età INTEGER,
+    Nazionalità VARCHAR(30) NOT NULL,
   );""")
 
 #Delete data from the table Clsh_Unit
-mycursor.execute("DELETE FROM CLASH_ROYALE.Clash_Unit")
+mycursor.execute("DELETE FROM Calciatori-Milan")
 mydb.commit()
 
 #Read data from a csv file
@@ -42,14 +37,14 @@ print(clash_data.head(20))
 for i,row in clash_data.iterrows():
     cursor = mydb.cursor()
     #here %S means string values 
-    sql = "INSERT INTO CLASH_ROYALE.Clash_Unit VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO Calciatori-Milan VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     cursor.execute(sql, tuple(row))
     print("Record inserted")
     # the connection is not auto committed by default, so we must commit to save our changes
     mydb.commit()
 
 #Check if the table has been filled
-mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit")
+mycursor.execute("SELECT * Calciatori-Milan")
 myresult = mycursor.fetchall()
 
 for x in myresult:

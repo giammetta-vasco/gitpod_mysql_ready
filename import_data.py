4de01b77d2656396c3a850ca357463e2e906b150
giammetta-vasco <giammetta.vasco@iisgalvanimi.edu.it>
2023-05-03 10:58:08 +0000

    Versione

--- import_data.py
+++ import_data.py
@@ -9,28 +9,30 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS Calciatori-Milan")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS Ac_Milan")
+
+mycursor.execute("DROP  TABLE IF  EXISTS Ac_Milan.Calciatori_Milan")
 
 #Create the table for the csv data (if not exists)
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS Calciatori-Milan (
+  CREATE TABLE IF NOT EXISTS Ac_Milan.Calciatori_Milan (
     Cognome VARCHAR(30) NOT NULL,
     Nome VARCHAR(30) NOT NULL,
     Ruolo VARCHAR(30) NOT NULL,
     Numero INTEGER,
-    Gol segnati INTEGER,
+    Gol_segnati INTEGER,
     Assist INTEGER,
-    Valore FLOAT,
-    Età INTEGER,
-    Nazionalità VARCHAR(30) NOT NULL,
+    Valore VARCHAR(30),
+    Eta INTEGER,
+    Nazionalita VARCHAR(30) NOT NULL
   );""")
 
 #Delete data from the table Clsh_Unit
-mycursor.execute("DELETE FROM Calciatori-Milan")
+mycursor.execute("DELETE FROM Ac_Milan.Calciatori_Milan")
 mydb.commit()
 
 #Read data from a csv file
-clash_data = pd.read_csv('./cr-unit-attributes.csv', index_col=False, delimiter = ',')
+clash_data = pd.read_csv('./Calciatori_Milan.csv', index_col=False, delimiter = ',')
 clash_data = clash_data.fillna('Null')
 print(clash_data.head(20))
 
@@ -38,14 +40,14 @@ print(clash_data.head(20))
 for i,row in clash_data.iterrows():
     cursor = mydb.cursor()
     #here %S means string values 
-    sql = "INSERT INTO Calciatori-Milan VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO Ac_Milan.Calciatori_Milan VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     cursor.execute(sql, tuple(row))
     print("Record inserted")
     # the connection is not auto committed by default, so we must commit to save our changes
     mydb.commit()
 
 #Check if the table has been filled
-mycursor.execute("SELECT * Calciatori-Milan")
+mycursor.execute("SELECT * FROM Ac_Milan.Calciatori_Milan")
 myresult = mycursor.fetchall()
 
 for x in myresult:
--- inutili/gpt.py
+++ inutili/gpt.py
@@ -4,20 +4,9 @@ conn = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database="Animali"
+  database="Ac-Milan"
 )
 
-cursor = conn.cursor()
-
-query = "INSERT INTO Mammiferi (name, specie) VALUES (%s, %s)"
-valori = [
-  ('Cane', 'Canide'),
-  ('Gatto', 'Felide'),
-  ('Leone', 'Felide'),
-  ('Orso', 'Urside'),
-  ('Tigre', 'Felide')
-]
-cursor.executemany(query, valori)
 
 conn.commit()
 
